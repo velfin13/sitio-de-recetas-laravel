3b3fe8f3559d460177033534be2b9375c5f9413a
Velfin <vvelasquez2465@utm.edu.ec>
2021-07-15 19:19:00 -0500

    sistema de login, rutas creado

---
+++ resources/views/recetas/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-center mb-5">Administra tus recetas</h2>
+    <div class="col-md-10 mx-auto bg-white p-3">
+        <table class="table">
+            <thead class="bg-primary text-light">
+                <tr>
+                    <th scole="col">Titulo</th>
+                    <th scole="col">Categoria</th>
+                    <th scole="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Italiana</td>
+                    <td>Pizza</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
