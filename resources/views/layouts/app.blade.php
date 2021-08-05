0357b08be5ca2a7d3e47d8f3617dc0e02f715958
Joseph Garcia <josephdgb1996@gmail.com>
2021-08-05 14:38:19 -0500

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,9 +30,7 @@
                 <a class="navbar-brand" href="{{ route('inicio.index') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -46,59 +44,54 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
+                        <li class="nav-item dropdown">
 
 
-                                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    @if (!Auth::user()->perfil->imagen)
-                                        <span
-                                            class="mr-2 d-none d-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                                    @else
-                                        <img class="img-profile rounded-circle" style="height: 40px;width: 40px;"
-                                            src="{{ '/storage/' . Auth::user()->perfil->imagen }}">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                @if (!Auth::user()->perfil->imagen)
+                                <span class="mr-2 d-none d-inline text-gray-600 small">{{ Auth::user()->name }}</span>
+                                @else
+                                <img class="img-profile rounded-circle" style="height: 40px;width: 40px;" src="{{ '/storage/' . Auth::user()->perfil->imagen }}">
 
-                                    @endif
+                                @endif
 
 
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                <a class="dropdown-item" href="{{ route('perfiles.show', ['perfil' => Auth::user()->id]) }}">
+                                    <i class="fa fa-user" aria-hidden="true"></i>
+                                    {{ __('Perfil') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                                    <a class="dropdown-item"
-                                        href="{{ route('perfiles.show', ['perfil' => Auth::user()->id]) }}">
-                                        <i class="fa fa-user" aria-hidden="true"></i>
-                                        {{ __('Perfil') }}
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('recetas.index') }}">
-                                        <i class="fa fa-cutlery" aria-hidden="true"></i>
-                                        {{ __('Ver Recetas') }}
-                                    </a>
+                                <a class="dropdown-item" href="{{ route('recetas.index') }}">
+                                    <i class="fa fa-cutlery" aria-hidden="true"></i>
+                                    {{ __('Ver Recetas') }}
+                                </a>
 
 
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal"
-                                        data-target="#logoutModal" onclick="event.preventDefault();
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal" onclick="event.preventDefault();
                                                                     document.getElementById('logout-form').submit();">
-                                        <i class="fa fa-sign-out" aria-hidden="true"></i>
-                                        {{ __('Logout') }}
-                                    </a>
+                                    <i class="fa fa-sign-out" aria-hidden="true"></i>
+                                    {{ __('Logout') }}
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -106,10 +99,12 @@
         </nav>
 
 
+        {{-- buscador --}}
+        @yield('hero')
+
         <nav class="navbar navbar-expand-md categorias-bg">
             <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#categorias"
-                    aria-controls="categorias" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#categorias" aria-controls="categorias" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                     Categorias
                 </button>
@@ -117,21 +112,17 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav w-100 d-flex justify-content-between">
                         @foreach ($categorias as $categoria)
-                            <li class="nav-item">
-                                <a class="nav-link"
-                                    href="{{ route('categorias.show', ['categoriaReceta' => $categoria->id]) }}">
-                                    {{ $categoria->nombre }}
-                                </a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('categorias.show', ['categoriaReceta' => $categoria->id]) }}">
+                                {{ $categoria->nombre }}
+                            </a>
 
-                            </li>
+                        </li>
                         @endforeach
                     </ul>
                 </div>
             </div>
         </nav>
-
-        {{-- buscador --}}
-        @yield('hero')
 
         {{-- seccion de botones --}}
         <div class="container">
