cf64e2fbe4599144e2770c3fe7097689b251d7f5
Velfin-Velasquez <vvelasquez2465@utm.edu.ec>
2021-08-09 15:58:32 -0500

    update

--- public/js/app.js
+++ public/js/app.js
@@ -66672,7 +66672,7 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "container" }, [
+  return _c("div", { staticClass: "container mt-5" }, [
     _c("div", { staticClass: "card" }, [
       _c("div", { staticClass: "card-body" }, [
         _c("h3", { staticClass: "heading" }, [
