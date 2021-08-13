f4d311ac0ac62bad8fb8922d4fd6f3accb43a547
Velfin-Velasquez <vvelasquez2465@utm.edu.ec>
2021-08-12 19:12:25 -0500

    vista de comentarios creada falta hacerla funcionar y crear las tablas

--- resources/views/recetas/show.blade.php
+++ resources/views/recetas/show.blade.php
@@ -82,13 +82,53 @@
 
             <div class="d-flex align-items-start">
                 @auth
-                    <calificacion-button id-user={{ Auth::user()->id }} id-receta={{ $recetas->id }}></calificacion-button>
+                    <calificacion-button id-user={{ Auth::user()->id }} id-receta={{ $recetas->id }}>
+                    </calificacion-button>
                 @else
-                    <calificacion-button id-user={{ "null" }} id-receta={{ $recetas->id }}></calificacion-button>
+                    <calificacion-button id-user={{ 'null' }} id-receta={{ $recetas->id }}></calificacion-button>
 
                 @endauth
             </div>
 
+
+            {{-- comentarios --}}
+            <div class="container mt-4">
+                <div class="row comments justify-content-center">
+                    <div class="col-6">
+                        @auth
+                            <form action="" class="form-comments d-flex justify-content-end flex-wrap">
+                                <textarea name="" id="" placeholder="Ingresa tu comentario"></textarea>
+                                <button type="submit" class="btn btn-primary">Comentar <i class="fa fa-paper-plane"
+                                        aria-hidden="true"></i></button>
+                            </form>
+                        @else
+                            <div class="alert alert-warning" role="alert">
+                                Para poder comentar debes <b><a href="{{ route('login') }}">Iniciar sesión</a></b>
+                            </div>
+
+                        @endauth
+
+                        <div class="row">
+                            <div class="media">
+                                <img src="http://news.toyark.com/wp-content/uploads/sites/4/2017/11/SHF-SSG-Goku-14.jpg"
+                                    width="64" height="64" alt="img">
+                                <div class="media-body">
+                                    <p class="user"><a href="javascript:;">Jose Maria Mera</a>
+                                    </p>
+                                    <p class="comment">
+                                        Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+                                        Malorum"
+                                        (The Extremes of Good and Evil) by Cicero, written in 45 BC.
+                                        This book is a treatise on the theory of ethics, very popular
+                                        during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor
+                                        sit amet..", comes from a line in section 1.10.32.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </article>
