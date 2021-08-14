34fe9779f00ab9f43e9f6ef9100f9ce89399c0b2
Velfin-Velasquez <vvelasquez2465@utm.edu.ec>
2021-08-14 18:21:03 -0500

    update

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -1,7 +1,6 @@
 {{-- user info and avatar --}}
-<div class="avatar av-l"><a href="/"><img width="100px" alt="100px" src="{{ asset('images/logo2.png') }}"
-            alt="logo"></a></div>
-<p class="info-name"><a href="/">{{ config('chatify.name') }}</a></p>
+<div class="avatar av-l"></div>
+<p class="info-name"><a href="#">{{ config('chatify.name') }}</a></p>
 <div class="messenger-infoView-btns">
     {{-- <a href="#" class="default"><i class="fas fa-camera"></i> default</a> --}}
     <a href="#" class="danger delete-conversation"><i class="fas fa-trash-alt"></i> Borrar conversación</a>
--- resources/views/vendor/Chatify/pages/app.blade.php
+++ resources/views/vendor/Chatify/pages/app.blade.php
@@ -16,46 +16,48 @@
             <input type="text" class="messenger-search" placeholder="Search" />
             {{-- Tabs --}}
             <div class="messenger-listView-tabs">
-                <a href="#" @if($route == 'user') class="active-tab" @endif data-view="users">
+                <a href="#" @if ($route == 'user') class="active-tab" @endif data-view="users">
                     <span class="far fa-user"></span> Personas</a>
-                <a href="#" @if($route == 'group') class="active-tab" @endif data-view="groups">
+                <a href="#" @if ($route == 'group') class="active-tab" @endif data-view="groups">
                     <span class="fas fa-users"></span> Grupos</a>
             </div>
         </div>
         {{-- tabs and lists --}}
         <div class="m-body">
-           {{-- Lists [Users/Group] --}}
-           {{-- ---------------- [ User Tab ] ---------------- --}}
-           <div class="@if($route == 'user') show @endif messenger-tab app-scroll" data-view="users">
+            {{-- Lists [Users/Group] --}}
+            {{-- ---------------- [ User Tab ] ---------------- --}}
+            <div class="@if ($route=='user' ) show @endif messenger-tab
+                app-scroll" data-view="users">
 
-               {{-- Favorites --}}
-               <div class="favorites-section">
-                <p class="messenger-title">Favoritos</p>
-                <div class="messenger-favorites app-scroll-thin"></div>
-               </div>
+                {{-- Favorites --}}
+                <div class="favorites-section">
+                    <p class="messenger-title">Favoritos</p>
+                    <div class="messenger-favorites app-scroll-thin"></div>
+                </div>
 
-               {{-- Saved Messages --}}
-               {!! view('Chatify::layouts.listItem', ['get' => 'saved','id' => $id])->render() !!}
+                {{-- Saved Messages --}}
+                {!! view('Chatify::layouts.listItem', ['get' => 'saved', 'id' => $id])->render() !!}
 
-               {{-- Contact --}}
-               <div class="listOfContacts" style="width: 100%;height: calc(100% - 200px);position: relative;"></div>
+                {{-- Contact --}}
+                <div class="listOfContacts" style="width: 100%;height: calc(100% - 200px);position: relative;"></div>
 
-           </div>
+            </div>
 
-           {{-- ---------------- [ Group Tab ] ---------------- --}}
-           <div class="@if($route == 'group') show @endif messenger-tab app-scroll" data-view="groups">
+            {{-- ---------------- [ Group Tab ] ---------------- --}}
+            <div class="@if ($route=='group' ) show @endif messenger-tab
+                app-scroll" data-view="groups">
                 {{-- items --}}
                 <p style="text-align: center;color:grey;">Soon will be available</p>
-             </div>
+            </div>
 
-             {{-- ---------------- [ Search Tab ] ---------------- --}}
-           <div class="messenger-tab app-scroll" data-view="search">
+            {{-- ---------------- [ Search Tab ] ---------------- --}}
+            <div class="messenger-tab app-scroll" data-view="search">
                 {{-- items --}}
                 <p class="messenger-title">Buscar</p>
                 <div class="search-records">
                     <p class="message-hint center-el"><span>Type to search..</span></p>
                 </div>
-             </div>
+            </div>
         </div>
     </div>
 
@@ -67,10 +69,10 @@
                 {{-- header back button, avatar and user name --}}
                 <div style="display: inline-flex;">
                     <a href="#" class="show-listView"><i class="fas fa-arrow-left"></i></a>
-                    <div class="avatar av-s header-avatar" style="margin: 0px 10px; margin-top: -5px; margin-bottom: -5px;">
-                        <a href="/"><img width="30px" alt="30px" src="{{ asset('images/logo2.png') }}" alt="logo"></a>
+                    <div class="avatar av-s header-avatar"
+                        style="margin: 0px 10px; margin-top: -5px; margin-bottom: -5px;">
                     </div>
-                    <a href="/" class="user-name">{{ config('chatify.name') }}</a>
+                    <a href="#" class="user-name">{{ config('chatify.name') }}</a>
                 </div>
                 {{-- header buttons --}}
                 <nav class="m-header-right">
