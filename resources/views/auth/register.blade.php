52f172cb44f9c903f3101fa82f1d9a4a32da4bb4
Joseph Garcia <josephdgb1996@gmail.com>
2021-08-23 17:53:48 -0500

    auth form changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,121 +1,88 @@
 @extends('layouts.auth')
 
 @section('auth')
-    <div class="container h-100 mt-3">
-        <div class="d-flex justify-content-center h-100">
-            <div class="user_card_register">
-                <div class="d-flex justify-content-center">
-                    <div class="brand_logo_container">
-                        <a href="{{ route('inicio.index') }}">
-                            <img src="{{ asset('images/logo2.png') }}" class="brand_logo" alt="Logo">
-                        </a>
-                    </div>
+<div class="container h-100 mt-3">
+    <div class="d-flex justify-content-center h-100">
+        <div class="user_card_register">
+            <div class="d-flex justify-content-center">
+                <div class="brand_logo_container">
+                    <a href="{{ route('inicio.index') }}">
+                        🥡
+                    </a>
                 </div>
-                <div class="d-flex justify-content-center form_container">
-                    <form method="POST" action="{{ route('register') }}" novalidate>
-                        @csrf
+            </div>
+            <div class="d-flex justify-content-center form_container">
+                <form method="POST" action="{{ route('register') }}" novalidate>
+                    @csrf
 
-                        {{-- name --}}
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
-                            </div>
+                    {{-- name --}}
+                    <div class="input-group mb-3">
+                        <input id="name" placeholder="Nombre" type="text" class="form-control input_user @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
 
-                            <input id="name" placeholder="Nombre" type="text"
-                                class="form-control input_user @error('name') is-invalid @enderror" name="name"
-                                value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    {{-- email --}}
+                    <div class="input-group mb-3">
 
-                            @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
+                        <input id="email" type="email" placeholder="ejemplo@dominio.com" class="form-control input_user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
 
-                        {{-- email --}}
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fa fa-envelope" aria-hidden="true"></i></span>
-                            </div>
+                    {{-- password --}}
+                    <div class="input-group mb-2">
+                        <input id="password" type="password" placeholder="Contraseña" class="form-control input_pass @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
 
-                            <input id="email" type="email" placeholder="Correo electronico"
-                                class="form-control input_user @error('email') is-invalid @enderror" name="email"
-                                value="{{ old('email') }}" required autocomplete="email">
-
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        {{-- password --}}
-                        <div class="input-group mb-2">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fa fa-lock" aria-hidden="true"></i></i></span>
-                            </div>
-
-                            <input id="password" type="password" placeholder="Contraseña"
-                                class="form-control input_pass @error('password') is-invalid @enderror" name="password"
-                                required autocomplete="new-password">
-
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        {{-- repeat password --}}
-                        <div class="input-group mb-2">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fa fa-lock" aria-hidden="true"></i></i></span>
-                            </div>
-                            <input id="password-confirm" type="password" class="form-control input_pass"
-                                placeholder="Repite la contraseña" name="password_confirmation" required
-                                autocomplete="new-password">
-                        </div>
+                    {{-- repeat password --}}
+                    <div class="input-group mb-2">
+                        <input id="password-confirm" type="password" class="form-control input_pass" placeholder="Contraseña" name="password_confirmation" required autocomplete="new-password">
+                    </div>
 
 
-                        {{-- url --}}
-                        <div class="input-group mb-3">
-                            <div class="input-group-append">
-                                <span class="input-group-text"><i class="fa fa-link" aria-hidden="true"></i></span>
-                            </div>
-                            <input id="url" type="url" class="form-control input_user @error('url') is-invalid @enderror"
-                                placeholder="url de tu sitio web" name="url" value="{{ old('url') }}" required
-                                autocomplete="url" autofocus>
+                    {{-- url --}}
+                    <div class="input-group mb-3">
+                        <input id="url" type="url" class="form-control input_user @error('url') is-invalid @enderror" placeholder="http://ejemplo.com" name="url" value="{{ old('url') }}" required autocomplete="url" autofocus>
+                        @error('url')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
 
-                            @error('url')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
+                    <div class="d-flex justify-content-center mt-3 login_container">
+                        <button type="submit" class="btn login_btn">
+                            {{ __('Register') }}
+                        </button>
+                    </div>
+                </form>
+            </div>
 
-                        <div class="d-flex justify-content-center mt-3 login_container">
-
-                            <button type="submit" class="btn login_btn">
-                                {{ __('Register') }}
-                            </button>
-
-                        </div>
-                    </form>
+            <div class="mt-4">
+                <div class="d-flex justify-content-center enlaces_auth">
+                    Ya tienes una cuenta? <a href="{{ route('login') }}" class="ml-2 btn-link" ><span class="login"><b>Inicia Sesión</b></span></a>
                 </div>
-
-                <div class="mt-4">
-                    <div class="d-flex justify-content-center enlaces_auth">
-                        Ya tienes una cuenta? <a href="{{ route('login') }}" class="ml-2 btn-link">Inicia Sesión</a>
-                    </div>
-                    <div class="d-flex justify-content-center enlaces_auth">
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
-                    </div>
+                <div class="d-flex justify-content-center enlaces_auth">
+                    @if (Route::has('password.request'))
+                    <a class="btn btn-link forgot_password" href="{{ route('password.request') }}">
+                       <b>{{ __('Forgot Your Password?') }}</b>
+                    </a>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
