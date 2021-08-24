6fd71d5704e54b352b99d4447f216c7794016989
Velfin-Velasquez <vvelasquez2465@utm.edu.ec>
2021-08-24 10:15:08 -0500

    update

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -6,9 +6,7 @@
 @endsection
 
 @section('botones')
-    <a href="{{ route('recetas.create') }}" class="btn btn-primary ml-2"><i class="fa fa-arrow-left"
-            aria-hidden="true"></i> Nueva</a>
-
+    @include('ui.navegacion')
 @endsection
 
 @section('content')
